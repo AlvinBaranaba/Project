4316e37358d8ae81469e5fa26fb398e843109079
AlvinBaranaba <baranabaalvin@gmail.com>
2025-12-10 08:44:51 +0300

    initial

--- poseifyapp/views.py
+++ poseifyapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
 
 # Create your views here.
 def home(request):
@@ -6,6 +7,15 @@ def home(request):
 def about(request):
     return render(request,'about.html')
 def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        subject=request.POST.get('subject')
+        message=request.POST.get('message')
+
+        query=Contact(name=name,email=email,subject=subject,message=message)
+        query.save()
+        return redirect("/")
     return render(request,'contact.html')
 def service(request):
     return render(request,'service.html')
